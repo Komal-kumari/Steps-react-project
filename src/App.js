70510d9eae7b308ab5d9a6ab7183f234894c8040
komal-kumari <kumarikomal941@gmail.com>
2023-10-02 01:53:04 +0530

    create steps react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useEffect, useState } from "react";
 
+const message = ["Learn React", "apply for jobs", "tell your incomes"];
 function App() {
+  // const step = 1;
+  const [step, setStep] = useState(1);
+  const stepsClick = () => {
+    step < 3 ? setStep(step + 1) : setStep(step);
+  };
+  const stepsPrevious = () => {
+    step > 1 ? setStep(step - 1) : setStep(step);
+  };
+  // const stepsClick = useEffect(() => {
+  //   setStep(step + 1);
+  //   // stepsClick();
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        Step: {step} {message[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={stepsPrevious}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={stepsClick}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
